8ac40584ddb9da1ba15f0bc64cab11ba6a226b24
Jahanvi Bhatt <jahanvi.bhatt@atqor.com>
2026-03-11 14:33:21 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+   <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind works</h1>
+    </div>
+  )
+}
+export default App
+
